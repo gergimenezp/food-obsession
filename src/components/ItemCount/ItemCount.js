60cdb47eed6a5b2c8a7cc6459b1116ddeba45af6
gergimenezp <gergimenez@hotmail.com>
2021-12-12 13:15:30 -0300

    contador con boton

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,30 @@
+import React, {useState} from "react";
+import './ItemCount.css';
+
+
+function ItemCount({initialValue, stock = 5}) {
+
+    const [value, setValue] = useState(initialValue);
+
+    const handleAdd = () => {
+        if(value < stock) setValue((p) => p + 1);
+    };
+
+    const handleSub = () => {
+        if(value > initialValue) setValue((p) => p - 1);
+    };
+
+    return(
+        <div>
+            <div className="producto">Aquí va mi producto</div>
+                <div className="count-container">
+                    <button className="btn-sub" onClick={handleSub}> sub - </button>
+                    <p>{value}</p>
+                    <button className="btn-add" onClick={handleAdd}> add + </button>
+                </div>
+            <button className="btn-toCart">Add to Cart</button>
+        </div>
+    );
+};
+
+export default ItemCount;
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Common from '../Common';
 import './ItemListContainer.css'
+import ItemCount from '../ItemCount/ItemCount'
 
 function Rack() {
     return(
-        <div>
+        <div className="contenedor">
             <Common.Label greeting="Hola, ¡bienvenido!" estilo="saludo"></Common.Label>
+            <ItemCount initialValue={1} stock={7} />
         </div>
     )
 }
