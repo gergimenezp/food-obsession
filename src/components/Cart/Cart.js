067fe2d8548e1b5d71d489b2b84d5e7dd359c16f
gergimenezp <gergimenez@hotmail.com>
2022-01-17 23:45:08 -0300

    Item collection II

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,5 @@
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
 import CartContext from '../../context/CartContext';
 import { Link } from "react-router-dom";
 import './Cart.css';
@@ -13,12 +14,52 @@ function Cart(){
 
     console.log(shoppingBasket);
 
+    const [orderId, setOrderId] = useState(0);
+
     const handleErase = () => {
         ValueContext.clear();
     }
 
     const handleRemove = (id) =>{
         ValueContext.removeItem(id)
+    }
+
+    const handlePlace = () => {
+
+        const order = {
+            buyer: {name: "Juan Carlos Gonzalez", phone: "555-234589", mail: "juancarlos@gonzalez.com"},
+            items: shoppingBasket.map((p)=>{
+                return{
+                    id: p.id,
+                    quantiy: p.quantity,
+                    title: p.name,
+                    price: p.price,
+                }
+            }),
+            total: totalToPay
+        };
+
+        const db = getFirestore();
+
+        const ordersCollection = collection(db, "orders");
+
+        addDoc(ordersCollection, order).then(({id}) => setOrderId(id));
+
+    }
+
+    const handleFinish = () => {
+        ValueContext.clear();
+        setOrderId(0);
+    }
+
+    if(orderId != 0){
+        return (
+            <div className="cart-detail">
+                <h2 className="cart-title">Thank you for your purchase</h2>
+                <h3>Your order id is {orderId}</h3>
+                <button onClick={handleFinish}>Finish</button>
+            </div>
+        )
     }
 
     return(
@@ -31,6 +72,7 @@ function Cart(){
                 </ul>
                 <h4 className="cart-total">Total: <span className="red">${totalToPay}</span> </h4>
                 <button onClick={handleErase}>Clear Cart</button>
+                <button onClick={handlePlace}>Place order</button>
                 
             </div>
         ) : (
